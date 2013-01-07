55d811e7cec3592bf1ed57a89860b4061668b45b
Jon Morehouse <morehousej09@gmail.com>
2013-01-07 05:51:33 -0500

    Updated directories

--- directories.sh
+++ directories.sh
@@ -1,3 +1,4 @@
+# basic navigation helpers
 alias ~='cd ~'
 alias ~='cd ~'
 alias .='cd ..'
@@ -6,55 +7,49 @@ alias ...='cd ../../..'
 alias ....='cd ../../../..'
 alias back='cd -'
 
-
-################## Document Navigation Helpers
-################## Document Navigation Helpers
-################## Document Navigation Helpers
-################## Document Navigation Helpers
-################## Document Navigation Helpers
+# basic directory navigation. Not necessarily code, media and general nav
 alias document='cd ~/Documents'
-alias programs='cd ~/Documents/programs'
+alias dotfiles='cd ~/dotfiles'
+alias amazon='cd ~/amazon'
 alias download='cd ~/Downloads'
 alias desktop='cd ~/Desktop'
-alias production_development='cd ~/Documents/production_development'
+alias dropbox='cd ~/Dropbox'
 alias music='cd ~/Music'
+alias picture='cd ~/Pictures'
+alias wd160='cd /Volumes/WD160/'
+alias work='cd ~/Documents/work'
+alias startups='cd ~/Documents/start_ups'
+
+
+# code specific files- scripts, programs, project roots etc
+alias programs='cd ~/Documents/programs'
+alias production_development='cd ~/Documents/production_development'
 alias general_development='cd ~/Documents/general_development'
 alias helper_programs='cd ~/Documents/general_development/helper_programs'
 alias bash_scripts='cd ~/Documents/general_development/helper_programs/bash'
 alias python_scripts='cd ~/Documents/general_development/helper_programs/python'
-alias github='cd ~/Documents/github' #this is the local github folder -- used to keep all versions off of servers
-alias work='cd ~/Documents/work'
-alias startups='cd ~/Documents/start_ups'
 alias testing_environments='general_development && cd testing_environments/'
 alias global_modules='general_development && cd modules'
 alias notes='general_development && cd notes/'
+
+
+# installation directories
 alias cellar='cd /usr/local/Cellar'
-alias wd160='cd /Volumes/WD160/'
+# apache
+# mysql
+# php 
 
 
-# Current PROJECT NAVIGATION
-# Current PROJECT NAVIGATION
-# Current PROJECT NAVIGATION
-# Current PROJECT NAVIGATION
-# Current PROJECT NAVIGATION
-# Current PROJECT NAVIGATION
-# Current PROJECT NAVIGATION
-alias prospero='production_development && cd prospero/current'
-alias ovai='production_development && cd ovai/'
-
-
-
-# CODEIGNITER SPECIFIC 
+# Framework specific nav helpers
 alias view='cd application/views/'
 alias model='cd application/models'
 alias controller='cd application/controllers'
 alias library='cd application/libraries'
 
-# DJANGO SPECIFIC
-alias media='cd ../media/'
-alias manage='python ./manage.py'
 
-
+# Projects I'm currently working on
+alias prospero='production_development && cd prospero/current'
+alias ovai='production_development && cd ovai/'
 alias momentum='cd /Users/MorehouseJ09/Documents/production_development/momentum/'
 alias _rails='cd /Users/MorehouseJ09/Documents/general_development/testing_environments/ruby/'
 
