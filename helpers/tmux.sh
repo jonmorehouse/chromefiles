d9d65e8f6b17b012340dd0513d9fb17f6a93f827
Jon Morehouse <morehousej09@gmail.com>
2013-01-21 03:59:57 -0800

    Updated tmux aliases

--- helpers/tmux.sh
+++ helpers/tmux.sh
@@ -1,11 +1,10 @@
 alias "tmux.new"='tmux new-session -s ${1}'
 alias "tmux.attach"='tmux attach-session -t ${1}'
-alias "tmux.destroy"='tmux attach-session -t ${1} && exit'
 alias "tmux.rename"='tmux attach-session -t ${1}'
 alias "tmux.list"='tmux list-sessions'
-alias "tmux.kill"='tmux kill-server'
+alias "tmux.kill_all"='tmux kill-server'
 alias "tmux.reload"='tmux source-file ~/.tmux.conf'
-
-# alias "tmux.send"='tmux send-keys -t "${1}" "${2}" C-m'
+alias "tmux.kill"='tmux kill-window -t ${1}'
+# alias "tmux.send"="tmux send-keys -t ${1} 'ls' C-m"
 
 # tmux list-windows -t $session|cut -d: -f1|xargs -I{} tmux send-keys -t $session:{} $message
