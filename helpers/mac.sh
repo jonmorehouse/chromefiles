e78db2b999da95c9fa7257a8b5d8eb82bfee7b1f
Jon Morehouse <morehousej09@gmail.com>
2013-02-22 17:03:30 -0800

    Updated mac notes

--- helpers/mac.sh
+++ helpers/mac.sh
@@ -1,1 +1,6 @@
+# lock the machine
 alias lock='/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend'
+
+# prevent the creation of ds_store files
+# defaults write com.apple.desktopservices DSDontWriteNetworkStores true
+
