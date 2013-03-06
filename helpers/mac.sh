adb958a6983da80b436a9f91bd91d8c0d01d0e28
Jon Morehouse <morehousej09@gmail.com>
2013-03-06 13:43:02 -0800

    Updated vim / tmux settings to allow for term=256 in tmux

--- helpers/mac.sh
+++ helpers/mac.sh
@@ -4,3 +4,4 @@ alias lock='/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resourc
 # prevent the creation of ds_store files
 # defaults write com.apple.desktopservices DSDontWriteNetworkStores true
 
+alias clean_ds='find / -name ".DS_Store" -depth'
