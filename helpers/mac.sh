f6bbdffcd0d8e74daa5c31259f7c13d297e8df78
Jon Morehouse <morehousej09@gmail.com>
2013-03-07 10:06:54 -0800

    Built out clean functionality for system wide removal of stray .DS_store and Icon\M files.

--- helpers/mac.sh
+++ helpers/mac.sh
@@ -5,3 +5,11 @@ alias lock='/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resourc
 # defaults write com.apple.desktopservices DSDontWriteNetworkStores true
 
 alias clean_ds='find / -name ".DS_Store" -depth'
+
+
+# clean up unwanted system files that cause problems when syncing to s3 / dropbox / copying in general
+function clean_system {
+
+	sudo find / -type f -name ".DS_Store" -or -name "Icon^M" -or -name "Icon?" -exec rm {} \;
+
+}
