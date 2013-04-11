5882bae89622c5166907d69567a14dca6cde1f76
Jon Morehouse <morehousej09@gmail.com>
2013-04-10 23:45:56 -0700

    Update plist launchd documentation / codebase

---
+++ launchd/install.sh
@@ -0,0 +1,2 @@
+Install script should be responsible for putting all scripts in their proper bin locations
+Should be responsible for installing all of the proper plist files as well as sufficient management for everything
