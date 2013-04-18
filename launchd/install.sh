a93d89408359ae6e6196e4384342d0d3bba59378
Jon Morehouse <morehousej09@gmail.com>
2013-04-18 00:07:47 -0700

    Update install script

--- launchd/install.sh
+++ launchd/install.sh
@@ -1,2 +1,16 @@
-Install script should be responsible for putting all scripts in their proper bin locations
-Should be responsible for installing all of the proper plist files as well as sufficient management for everything
+#Install script should be responsible for putting all scripts in their proper bin locations
+#Should be responsible for installing all of the proper plist files as well as sufficient management for everything
+
+files=`find plist/*.plist -not -name "*example*"`
+
+link() {
+
+	echo ${1}
+}
+
+# loop through all of the files etc and 
+for file in $files
+do
+	echo $file	
+done
+
