005b75b7f6b099be881690778fa3705efea339d3
Jon Morehouse <morehousej09@gmail.com>
2013-04-19 23:14:04 -0700

    Update install script for launchdaemon files

--- launchd/install.sh
+++ launchd/install.sh
@@ -11,17 +11,34 @@ root_directory=/System/Library/LaunchDaemons
 # reference url for site
 # https://developer.apple.com/library/mac/#documentation/Darwin/Reference/ManPages/man5/launchd.plist.5.html
 
-
 link() {
 
 	# want to check if the file exists and if it doe, remove the ln
 	# then ln our new element
+	# first check if the element is a root script or not
+	if [[ $1 == *_root.plist ]]; then
+	 	#statements
+	 	file=$root_directory/${1}
+	else
+		file=$directory/${1}
+	fi 
 
+	# now that we have the proper file name ... make sure that the file doesn't exist
+	if [[ -e $file ]]; then
+
+		# print a clever message here ...
+		echo "$file already exists..."
+
+	# otherwise lets actually symlink it already
+	else
+		# symlink the proper files to their location on the machine etc
+		ln -s $1 $file
+	fi 
 }
 
 # loop through all of the files etc and 
 for file in $files
 do
-	link $file
+	link "${file#plist/*}"
 done
 
