ac3d4515062b4f7548b2996321dee71952e350f6
Jon Morehouse <morehousej09@gmail.com>
2013-04-27 21:58:30 -0700

    Update paths for custom php installation

--- bash_config/paths.sh
+++ bash_config/paths.sh
@@ -1,4 +1,3 @@
-PATH=:/usr/local/share/npm/bin:/Users/MorehouseJ09/Resources/helper_programs:/Users/MorehouseJ09/Resources/programs:/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/local/php/bin:/usr/local/Cellar/php54/5.4.5/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/opt/X11/bin:/usr/local/apache2/bin:/usr/local/nginx/sbin:/usr/local/ec2-api-tools/bin:/usr/local/src/jpeg-8c
-
+PATH=:/usr/local/share/npm/bin:/Users/MorehouseJ09/Resources/helper_programs:/Users/MorehouseJ09/Resources/programs:/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/local/php/bin:/usr/local/Cellar/php54/5.4.5/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/opt/X11/bin:/usr/local/apache2/bin:/usr/local/nginx/sbin:/usr/local/ec2-api-tools/bin:/usr/local/src/jpeg-8c:/usr/local/php/bin
 
 export PATH
