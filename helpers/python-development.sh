b3cd9cca0c49b5e1abf8f5232427df8c5398502f
Jon Morehouse <morehousej09@gmail.com>
2014-01-15 01:31:05 -0800

    Update python development workflow. Move shell config scripts to dotfiles project

--- helpers/python-development.sh
+++ helpers/python-development.sh
@@ -1,6 +1,10 @@
 alias nose='nosetests --with-watch --nocapture .'
 alias clear-virtualenv='rm -rf bin/ build/ include/ lib/'
+alias install-virtualenv='pip install --ignore-installed -r requirements.txt --src eggs'
 alias count-python='cloc --exclude-dir=bin,lib,include,src/gevent . '
+alias fabll='fab --list-format=nested --list'
 alias fabl='fab --list'
+alias nose='nosetests --with-watch --nocapture --nologcapture .'
+
 
 
