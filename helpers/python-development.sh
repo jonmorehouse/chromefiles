18307544aab56a18a6f41989cea41ba358f9b2c7
Jon Morehouse <morehousej09@gmail.com>
2014-01-15 21:19:32 -0800

    Update gitignore global settings

--- helpers/python-development.sh
+++ helpers/python-development.sh
@@ -1,10 +1,21 @@
 alias nose='nosetests --with-watch --nocapture .'
 alias clear-virtualenv='rm -rf bin/ build/ include/ lib/'
-alias install-virtualenv='pip install --ignore-installed -r requirements.txt --src eggs'
 alias count-python='cloc --exclude-dir=bin,lib,include,src/gevent . '
 alias fabll='fab --list-format=nested --list'
 alias fabl='fab --list'
 alias nose='nosetests --with-watch --nocapture --nologcapture .'
+
+function install-virtualenv {
+
+	for i in `ls *requirements.txt` 
+	do
+		echo $i
+		pip install --ignore-installed -r $i --src eggs
+	done
+}
+
+
+
 
 
 
