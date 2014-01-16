d574c23cf7352aae4554d7dc4e437b2a13858652
Jon Morehouse <morehousej09@gmail.com>
2014-01-15 21:56:25 -0800

    Build out further python development needs as library development continues

--- helpers/python-development.sh
+++ helpers/python-development.sh
@@ -1,11 +1,35 @@
 alias nose='nosetests --with-watch --nocapture .'
-alias clear-virtualenv='rm -rf bin/ build/ include/ lib/'
 alias count-python='cloc --exclude-dir=bin,lib,include,src/gevent . '
 alias fabll='fab --list-format=nested --list'
 alias fabl='fab --list'
-alias nose='nosetests --with-watch --nocapture --nologcapture .'
+alias nose='nosetests --nocapture --nologcapture .'
+
+# clear a virtualenvironement after use
+function clear-virtualenv {
+
+	# remove virtualenv dependencies
+	rm -rf bin/ build/ include/ lib/
+	rm -rf man
+	rm -rf .Python
+
+	# now remove egg files as needed
+	rm -rf *egg(N)
+	rm -rf *egg-info(N)
+
+}
 
 function install-virtualenv {
+
+	# make sure we are in a virtual environment first
+	if [ ! -d bin ]
+	then
+
+		virtualenv . 
+
+	fi
+
+	# source teh element
+	source bin/activate
 
 	for i in `ls *requirements.txt` 
 	do
