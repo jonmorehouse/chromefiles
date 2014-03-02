37b820dc87953b52b26053080c14cba65f244cae
Jon Morehouse <morehousej09@gmail.com>
2014-03-01 21:44:27 -0800

    Remove alias_generator and move it ot a a script

--- helpers/shell-functions.sh
+++ helpers/shell-functions.sh
@@ -1,35 +1,3 @@
-function alias_generator {
-	# generate the alias path as needed
-	alias_path="$1"
-	if [ $# -gt 1 ]
-	then 
-		# if _ or "" is passed in, then we want to just pass in a blank
-		if [[ $2 == "_" || $2 == "" ]]
-		then
-			alias_prefix=""
-		else
-			alias_prefix="$2-"
-		fi
-	else 
-		alias_prefix="$(echo $1 | awk '{ n=split($0,a,"/"); print tolower(a[n]) }')-"
-	fi
-	
-	# create parent alias -- unless flag is passed
-	if [ ! $# -eq 3 ] 
-	then
-		parent=$(echo $alias_prefix | sed 's/-//')
-		eval alias ${parent}='${alias_path}'
-	fi
-
-	# generate and export the aliases as needed
-	for i in $(ls "$alias_path")
-	do
-		alias_name="$alias_prefix$i"
-		alias_function="cd $alias_path/$i"
-		eval alias ${alias_name}='${alias_function}'
-	done
-}
-
 function remove_function {
 
     type $1 | grep -q 'shell function'
